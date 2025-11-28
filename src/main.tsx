03463af5aa4b64af833d4e2ce71d6b0c75dfa1ba
Emmanuel Ejereje Uchivwoghene <uchivwoghene@gmail.com>
2025-11-28 21:57:14 +0100

    Initialise csrf token before app load

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { initCsrf } from "./api/axiosInstance";
 import App from "./App";
 import "./index.css"; // tailwind already imported here
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+initCsrf().then(() => {
+  ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  );
+});
